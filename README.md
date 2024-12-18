c91ecb1c3b8defe9cad2656fc2745cebf4aa3056
lanslord11 <piyushyadav11102002@gmail.com>
2024-12-18 16:30:32 +0530

    initla setup

---
+++ README.md
@@ -0,0 +1,15 @@
+# Web Scraper with Puppeteer and Next.js API Routes
+
+Demo for tutorial [Build a Web Scraper with Puppeteer & Next.js API Routes](https://www.youtube.com/watch?v=kOdIzhPfLuo)
+
+📝 Article: https://spacejelly.dev/posts/build-a-web-scraper-with-puppeteer-next-js-api-routes
+
+📺 YouTube: https://www.youtube.com/watch?v=kOdIzhPfLuo
+
+## More tutorials and walkthroughs
+
+🐦 [Follow me on Twitter](https://twitter.com/colbyfayock)
+
+📺 [Subscribe on YouTube](https://www.youtube.com/colbyfayock)
+
+✉️ [Sign Up for My Newsletter](https://colbyfayock.com/newsletter)
---
+++ src/app/api/scraper/route.ts
@@ -0,0 +1,47 @@
+import chromium from '@sparticuz/chromium-min';
+import puppeteer from 'puppeteer-core';
+import { v2 as cloudinary } from 'cloudinary';
+
+// cloudinary.config({
+//   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
+
+export const maxDuration = 59;
+
+export async function POST(request: Request) {
+  const { siteUrl } = await request.json();
+
+  const isLocal = !!process.env.CHROME_EXECUTABLE_PATH;
+
+  const browser = await puppeteer.launch({
+    args: isLocal ? puppeteer.defaultArgs() : chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath(process.env.CHROME_EXECUTABLE_PATH),
+    headless: chromium.headless,
+  });
+
+  const page = await browser.newPage();
+  await page.goto(siteUrl);
+  const pageTitle = await page.title();
+  const screenshot = await page.screenshot();
+  await browser.close();
+
+  // const resource = await new Promise((resolve, reject) => {
+  //   cloudinary.uploader.upload_stream({}, function (error: unknown, result: unknown) {
+  //     if (error) {
+  //       reject(error);
+  //       return;
+  //     }
+  //     resolve(result);
+  //   })
+  //   .end(screenshot);
+  // });
+
+  return Response.json({
+    siteUrl,
+    pageTitle,
+    // resource
+  })
+}
